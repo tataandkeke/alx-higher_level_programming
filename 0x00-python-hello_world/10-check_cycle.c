f69a8040b099c09aac032a0fedb158292eafd844
richard <richardoladiran@gmail.com>
2023-03-06 07:34:00 -0800

    last task

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * check_cycle - list checking
+ * @list: list
+ * Return: 0 or 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	(void)list;
+	return (1);
+}
