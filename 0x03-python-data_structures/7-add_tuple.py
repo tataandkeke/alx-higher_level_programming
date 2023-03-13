ec50da0a166de8bc036f34dbdaabde01f953015a
richard <richardoladiran@gmail.com>
2023-03-13 06:43:08 -0700

    Aded task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a_1 = tuple_a[0]
-    tuple_a_2 = tuple_a[1]
-    tuple_b_1 = tuple_b[0]
-    tuple_b_2 = tuple_b[1]
-    sum_1 = tuple_a_1 + tuple_b_1
-    sum_2 = tuple_a_2 + tuple_b_2
-    new_tuple = (sum_1, sum_2)
-    return new_tuple
+def add_tuple(tuple_a=(), tuple_b=()):
+    a = list(tuple_a) + [0]*2
+    b = list(tuple_b) + [0]*2
+    c = [x + y for x, y in zip(a, b)]
+    return tuple(c[0:2])
