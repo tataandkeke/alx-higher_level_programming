3fe676f745cd275207a2e3ed13a38d1d7553a33d
richard <richardoladiran@gmail.com>
2023-03-13 07:52:47 -0700

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_palindrome - checking for palindrom
+ * @head: linked list header pointer
+ *
+ * Description: checking for list
+ * Return: 1 or 0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	(void) head;
+	return (1);
+}
