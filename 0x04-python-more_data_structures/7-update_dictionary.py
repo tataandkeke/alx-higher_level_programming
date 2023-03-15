a05bff838e5b89ca0f2f74e635675e8fb6440bde
richard <richardoladiran@gmail.com>
2023-03-15 06:24:22 -0700

    Fixed task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def update_dictionary(a_dictionary, key, value):
-    for x in a_dictionary.keys():
-        if x == key:
-            a_dictionary[x] = value
-        else:
-            a_dictionary[key] = value
-    return a_dictionary
+def update_dictionary(my_dict, key, value):
+    my_dict[key] = value
+    return my_dict
