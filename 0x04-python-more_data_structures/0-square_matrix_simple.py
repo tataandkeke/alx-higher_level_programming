98426bdcbac5065969124c9a395aa24f41f4c071
richard <richardoladiran@gmail.com>
2023-03-15 05:23:53 -0700

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in range(len(matrix)):
+        new_list.append(list(map(lambda x: x**2, matrix[i])))
+    return new_list
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This is a project about dictionaries and sets
