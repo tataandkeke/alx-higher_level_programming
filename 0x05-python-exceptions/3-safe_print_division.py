6530d99b204145921736d6d8974b76d1e675bb8f
richard <richardoladiran@gmail.com>
2023-03-27 05:23:04 -0700

    task3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a / b
+        diva = a / b
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside reslt: {}".format(div))
+        print("Inside reslt: {}".format(diva))
