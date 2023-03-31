3a1ef44f7f8c8506b9ca1caad785712497aef800
richard <richardoladiran@gmail.com>
2023-03-31 10:58:58 -0700

    adv task 0

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" add_integer module
+    adds 2 numbers and retunrs result
+    """
+
+
+def matrix_mul(m_a, m_b):
+    """Adds 2 numbers a float or integet
+
+
+    Arguments:
+        a (int, float): first arguement
+        b (int, float, optional): default value 98
+
+    Raises:
+        TypeError: a must be an integer
+        TypeError: b must be an integer
+
+    Returns:
+        int value of the sum of a + b
+    """
