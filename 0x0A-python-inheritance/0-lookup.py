f8d441783b08582dcb342d34591f782a35591fd9
richard <richardoladiran@gmail.com>
2023-04-11 04:44:19 -0700

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
+"""Lookup module
+"""
+
+
 def lookup(obj):
+    """ returns the list of all methods in the object
+    Args:
+        obj: object variable
+    Return:
+        List of methods in the object
+    """
+
     return (dir(obj))
