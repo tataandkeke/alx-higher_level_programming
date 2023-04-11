755d33ff076006eb6ad40cdf4a4d32cfa8117484
richard <richardoladiran@gmail.com>
2023-04-11 05:58:18 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """ empty BaseGeometry
+    """
+
+    def area(self):
+        """Area function that is still empty
+
+        Args:
+            self: this object
+        Return:
+            return area
+        """
+
+        raise Exception("area() is not implemented")
