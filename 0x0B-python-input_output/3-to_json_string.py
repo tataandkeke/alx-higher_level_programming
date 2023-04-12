2d2d29fe5fdbc94a496406a18611acfa1a107559
richard <richardoladiran@gmail.com>
2023-04-12 07:00:06 -0700

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is the to_json_string module
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """ to_json_string returns the json rep of an object
+
+        Args:
+            my_obj: object variable
+        Return:
+            the JSON of the object
+    """
+
+    return (json.dumps(my_obj))
