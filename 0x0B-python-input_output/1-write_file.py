925d406801b252750af185503360170ca2985b5d
richard <richardoladiran@gmail.com>
2023-04-12 06:35:51 -0700

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This is the write_file module
+"""
+
+
+def write_file(filename="", text=""):
+    """ write_file function write a file and prints it to stdot
+
+        Args:
+            filename: file name variable
+            text:     characters to write into file
+        Return:
+            reads the file
+    """
+
+    n_char = 0
+
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        s_file.write(text)
+
+    for c in text:
+        n_char += 1
+
+    return (n_char)
